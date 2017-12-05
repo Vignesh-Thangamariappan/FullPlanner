58fa28dec4be6562be703206c068b5a702e2eb7c
Vignesh-Thangamariappan <user@full368-air.full.creative>
2017-12-05 18:49:30 +0530

    Implemented Custom Login Button

--- MyAssistant/Maps.swift
+++ MyAssistant/Maps.swift
@@ -16,33 +16,44 @@ class Maps: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
+        mapView.delegate = self
+        let locationChennai = CLLocationCoordinate2DMake(13.0827, 80.2707)
+        mapView.setRegion(MKCoordinateRegionMakeWithDistance(locationChennai, 1500, 1500), animated: true)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-    override func viewDidAppear(_ animated: Bool) {
-        
-    }
+    
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(true)
+//        getLocation()
+//    }
+    
     
     func getLocation() {
         
         let locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestAlwaysAuthorization()
         
-        if CLLocationManager.locationServicesEnabled() {
-            
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
             locationManager.startUpdatingLocation()
         }
+        
     }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .denied || status != .restricted || status != .notDetermined {
+            manager.startUpdatingLocation()
+        }
+    }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation:CLLocation = locations[0] as CLLocation
+        manager.stopUpdatingLocation()
         let center = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
         
         let region = MKCoordinateRegionMakeWithDistance(center, 1500, 1500)
@@ -52,5 +63,36 @@ class Maps: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("error =\(error)")
+    }
+    @IBAction func didLongPress(_ sender: UILongPressGestureRecognizer) {
+        let touchPoint = sender.location(in: mapView)
+        let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: self.mapView)
+        let pin = MKPointAnnotation()
+        pin.coordinate = touchCoordinate
+        
+//        print("\(touchCoordinate.latitude) + \(touchCoordinate.longitude)")
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: touchCoordinate.latitude, longitude: touchCoordinate.longitude)
+        
+        geoCoder.reverseGeocodeLocation(location, completionHandler: { placemarks, error in
+            guard let addressDict = placemarks?[0].addressDictionary  else {
+                return
+            }
+            
+            // Print each key-value pair in a new row
+            addressDict.forEach { print($0) }
+            
+            // Print fully formatted address
+            if let formattedAddress = addressDict["FormattedAddressLines"] as? [String] {
+                print(formattedAddress.joined(separator: ", "))
+            }
+            
+            // Access each element manually
+            if let locationName = addressDict["Name"] as? String {
+                print(locationName)
+                pin.title = locationName
+            }
+            self.mapView.addAnnotation(pin)
+        })
     }
 }
